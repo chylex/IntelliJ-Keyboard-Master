d6053046a5555c7a82f4c8a186574462e9d6af10
chylex <contact@chylex.com>
2021-08-21 15:55:40 +0200

    Add main project files

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+	kotlin("jvm") version "1.5.10"
+	id("org.jetbrains.intellij") version "1.1.4"
+	java
+}
+
+group = "com.chylex.intellij.keyboardmaster"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+intellij {
+    version.set("2021.2")
+}
