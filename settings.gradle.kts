c56e8ca306678b62f65f2d2eaf87577233750e59
chylex <contact@chylex.com>
2026-08-26 16:52:58 +0200

    Update Gradle to 9.7.0 and IntelliJ Platform plugin to 2.18.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "KeyboardMaster"
 
 pluginManagement {
 	plugins {
-		kotlin("jvm") version "1.9.24"
-		id("org.jetbrains.intellij.platform") version "2.9.0"
+		kotlin("jvm") version "1.9.24" // https://plugins.jetbrains.com/docs/intellij/using-kotlin.html#bundled-stdlib-versions
+		id("org.jetbrains.intellij.platform") version "2.18.1" // https://github.com/JetBrains/intellij-platform-gradle-plugin/releases
 	}
 }
